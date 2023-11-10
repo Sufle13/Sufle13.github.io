bd9c82a68700edbe2ed84df1125a71a2eb22fd53
Фломастер <57543730+Sufle13@users.noreply.github.com>
2023-11-10 16:54:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-let btn_m = document.getElementById("btn");
-let btn_v = document.getElementById("btn_v");
+let btn_m = document.getElementById('max_btn');
+let btn_v = document.getElementById('val_btn');
 
 btn_m.addEventListener("click", function(){
-    tg.sendData("you are fed Valera");
+    tg.sendData("you are fed Maxim");
 });
 
 btn_v.addEventListener("click", function(){
