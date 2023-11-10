b5fb3c4ff9cb34e9eaee882b2ee625b0e6c3685a
Фломастер <57543730+Sufle13@users.noreply.github.com>
2023-11-10 15:24:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#FF00FF";
-
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function(){
