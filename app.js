5eff5ef0a79861206edd14898e5dffb6aa8fdd04
Фломастер <57543730+Sufle13@users.noreply.github.com>
2023-11-14 15:21:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+var MainButton = WebApp.MainButton;
+var BackButton = WebApp.BackButton;
 let tg = window.Telegram.WebApp;
 
 tg.expand();
+MainButton.show();
+BackButton.show();
 
 let btn_m = document.getElementById('max_btn');
 let btn_v = document.getElementById('val_btn');
@@ -11,4 +15,22 @@ btn_m.addEventListener("click", function(){
 
 btn_v.addEventListener("click", function(){
     tg.sendData("you are fed Valera");
+});
+
+
+MainButton.onClick(function() {
+  WebApp.showAlert("Хорошо, ты нажал на главную кнопку.");
+});
+
+WebApp.onEvent('mainButtonClicked', function() {
+  /* also */
+});
+
+BackButton.onClick(function() {
+  WebApp.showAlert("Нет пути назад!");
+  
+  BackButton.hide();
+});
+WebApp.onEvent('backButtonClicked', function() {
+  /* also */
 });
